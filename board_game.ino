2c5ffed83f62f56e8aede503a84254c7fff776d1
Claves do Amaral <claves.doamaral@gmail.com>
2025-06-02 20:30:19 +0100

    More object oriented

--- board_game.ino
+++ board_game.ino
@@ -1,8 +1,5 @@
 #include <LiquidCrystal.h>
 
-const int BOSS_ENERGY = 10;
-const int PLAYER_POINTS = 10;
-const int PATH_LIGHTS = 10;
 const int BUTTON_PIN = 3;
 const int displayWidth = 16;
 
@@ -11,48 +8,49 @@ const int displayWidth = 16;
 const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
 const LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// an array of pin numbers to which the boss' LEDs are attached
-int bossEnergyPins[] = {
-  22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+// represents a led array
+struct LedArray {
+  LedArray(const int* _pins, int _size)
+    : pins(_pins), size(_size){};
+  // set index, returns the current setting
+  int setIndex(int newIndex) {
+    int current = index;
+    index = newIndex % size;
+    return current;
+  }
+  const int* pins;
+  int size;
+  int index = 0;
 };
 
-// an array of pin numbers to which player LEDs are attached
-int playerPointsPins[] = {
-  32, 33, 34, 35, 36, 37, 38, 39, 40, 41
-};
+// energy leds
+const int energyPins[] = { 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+const LedArray bossEnergyLeds = LedArray(energyPins, sizeof(energyPins) / sizeof(energyPins[0]));
+// player leds
+const int playerPins[] ={ 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 };
+const LedArray playerPointsLeds = LedArray(playerPins, sizeof(playerPins) / sizeof(playerPins[0]));
+// path leds
+const int pathPins[] =  { 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
+const LedArray pathLeds = LedArray(pathPins, sizeof(pathPins) / sizeof(pathPins[0]));
 
-// an array of pin numbers to which path LEDs are attached
-int pathLightPins[] = {
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51
-};
-
+// manages the button events
 volatile byte buttonReleased = false;
 void buttonReleasedInterrupt() {
   buttonReleased = true;
 }
 
-void setUpPins(const int* pins, int size) {
+void setUpPins(const LedArray& leds) {
   // loop over the pin array and set them all to output:
-  for (int idx = 0; idx < size; idx++) {
-    pinMode(pins[idx], OUTPUT);
+  for (int idx = 0; idx < leds.size; idx++) {
+    pinMode(leds.pins[idx], OUTPUT);
   }
 }
 
-void fillUpEnergy(const int* pins, int size) {
-  // loop over the LED array:
-  for (int idx = 0; idx < size; idx++) {
-    digitalWrite(pins[idx], HIGH);
-    delay(200);
+void putOnLed(const LedArray& leds, int index, bool putOffPrev = true) {
+  if (putOffPrev) {
+    digitalWrite(leds.pins[leds.setIndex(index)], LOW);
   }
-}
-
-void loopPins(const int* pins, int size) {
-  // loop over the LED array:
-  for (int idx = 0; idx < size; idx++) {
-    digitalWrite(pins[idx], HIGH);
-    delay(200);
-    digitalWrite(pins[idx], LOW);
-  }
+  digitalWrite(leds.pins[leds.index], HIGH);
 }
 
 void blinkText(const String& message, int row = 0, int repetitions = 2, int delayMillis = 800) {
@@ -90,9 +88,9 @@ void scrollText(const String& message, int row = 0, int delayMillis = 300) {
 
 void setup() {
   // setup led pins
-  setUpPins(bossEnergyPins, BOSS_ENERGY);
-  setUpPins(playerPointsPins, PLAYER_POINTS);
-  setUpPins(pathLightPins, PATH_LIGHTS);
+  setUpPins(bossEnergyLeds);
+  setUpPins(playerPointsLeds);
+  setUpPins(pathLeds);
 
   Serial.begin(9600);
   // setup button
@@ -117,7 +115,7 @@ void loop() {
     Serial.println("Button released");
   }
   delay(100);
-  fillUpEnergy(bossEnergyPins, BOSS_ENERGY);
+  // fillUpEnergy(bossEnergyPins, BOSS_ENERGY);
   // loopPins(playerPointsPins, PLAYER_POINTS);
   // loopPins(pathLightPins, PATH_LIGHTS);
 
