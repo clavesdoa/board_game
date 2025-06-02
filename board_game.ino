6163cbdb8524a4f8106ec31c392db3e88699b2dc
Claves do Amaral <claves.doamaral@gmail.com>
2025-06-03 00:12:15 +0100

    Added timer to make it async

--- board_game.ino
+++ board_game.ino
@@ -1,12 +1,8 @@
 #include <LiquidCrystal.h>
+#include <AsyncTimer.h>
 
 const int BUTTON_PIN = 3;
 const int displayWidth = 16;
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
-const LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // represents a led array
 struct LedArray {
@@ -23,14 +19,33 @@ struct LedArray {
   int index = 0;
 };
 
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
+const LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// timer
+AsyncTimer t;
+unsigned long timeLine = 0;
+// schedule function call
+template<typename Func>
+unsigned long schedule(Func f, unsigned long delay) {
+  timeLine += delay;
+  return t.setTimeout([&]() {
+    // call the lambda
+    f();
+  },
+                      timeLine);
+}
+
 // energy leds
 const int energyPins[] = { 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
 const LedArray bossEnergyLeds = LedArray(energyPins, sizeof(energyPins) / sizeof(energyPins[0]));
 // player leds
-const int playerPins[] ={ 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 };
+const int playerPins[] = { 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 };
 const LedArray playerPointsLeds = LedArray(playerPins, sizeof(playerPins) / sizeof(playerPins[0]));
 // path leds
-const int pathPins[] =  { 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
+const int pathPins[] = { 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
 const LedArray pathLeds = LedArray(pathPins, sizeof(pathPins) / sizeof(pathPins[0]));
 
 // manages the button events
@@ -53,21 +68,25 @@ void putOnLed(const LedArray& leds, int index, bool putOffPrev = true) {
   digitalWrite(leds.pins[leds.index], HIGH);
 }
 
-void blinkText(const String& message, int row = 0, int repetitions = 2, int delayMillis = 800) {
+void blinkText(const String& message, int row = 0, int repetitions = 2, unsigned long delayMillis = 1000) {
   lcd.clear();
   lcd.setCursor(0, row);
   lcd.print(message);
-  for (int idx = 0; idx < repetitions; idx++) {
-    // Turn off the display:
-    lcd.noDisplay();
-    delay(delayMillis);
-    // Turn on the display:
-    lcd.display();
-    delay(delayMillis);
+  for (int idx = 1; idx <= repetitions; idx++) {
+    schedule([]() {
+      // Turn off the display:
+      lcd.noDisplay();
+    },
+             delayMillis * idx);
+    schedule([]() {
+      // Turn on the display:
+      lcd.display();
+    },
+             delayMillis * idx);
   }
 }
 
-void scrollText(const String& message, int row = 0, int delayMillis = 300) {
+void scrollText(const String& message, int row = 0, unsigned long delayMillis = 300) {
   if (row == 0) {
     lcd.clear();
   }
@@ -77,13 +96,24 @@ void scrollText(const String& message, int row = 0, int delayMillis = 300) {
     lcd.setCursor(0, row);
     lcd.print(message);
   } else {
-    for (int i = 0; i <= segmentSize; i++) {
-      String segment = message.substring(i, i + displayWidth);
-      lcd.setCursor(0, row);
-      lcd.print(segment);
-      delay(delayMillis);
+    for (int idx = 0; idx <= segmentSize; idx++) {
+      schedule([&]() {
+        String segment = message.substring(idx, idx + displayWidth);
+        lcd.setCursor(0, row);
+        lcd.print(segment);
+      },
+               delayMillis * (idx + 1));
     }
   }
+}
+
+void runIntro() {
+  blinkText("Hello Players!");
+  // scrollText("Hello Players!  Welcome to the Game!");
+  // delay(1000);
+  // scrollText("Ready to play?");
+  // delay(1000);
+  // scrollText("Push the button", 1);
 }
 
 void setup() {
@@ -100,15 +130,14 @@ void setup() {
                   FALLING);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  blinkText("Hello Players!");
-  scrollText("Hello Players!  Welcome to the Game!");
-  delay(1000);
-  scrollText("Ready to play?");
-  delay(1000);
-  scrollText("Push the button", 1);
 }
 
 void loop() {
+  // hadle timer
+  t.handle();
+  timeLine = 0;
+  runIntro();
+  // handle button
   if (buttonReleased) {
     buttonReleased = false;
     // do something here, for example print on Serial
